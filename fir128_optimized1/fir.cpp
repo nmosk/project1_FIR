79d115cbc28550a7e7d420e2c1bd510df8201233
nmosk <mosknj@gmail.com>
2023-10-23 23:53:36 -0700

    Add files via upload

---
+++ fir128_optimized1/fir.cpp
@@ -0,0 +1,50 @@
+/*
+	Filename: fir.cpp
+		FIR lab wirtten for WES/CSE237C class at UCSD.
+		Match filter
+	INPUT:
+		x: signal (chirp)
+
+	OUTPUT:
+		y: filtered output
+
+*/
+
+#include "fir.h"
+
+void fir (
+  data_t *y,
+  data_t x
+  )
+{
+
+	//#pragma HLS pipeline ii=110
+
+
+	long long c[N] = {10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+	
+	// Write your code here
+	static long long my_reg[N];
+#pragma HLS ARRAY_PARTITION variable=my_reg type=complete
+	long long acc;
+	int i;
+	acc = 0;
+
+	for (i = N-1; i > 0; i--){
+//#pragma HLS unroll factor=32
+			my_reg[i] = my_reg[i - 1];
+		}
+
+	for (i = N-1; i > 0; i--){
+//#pragma HLS unroll factor=32
+			acc += my_reg[i] * c[i];
+		}
+		acc += (long long) x * c[0];
+		my_reg[0] = (long long) x;
+
+	*y = acc;
+
+
+}
+
+
